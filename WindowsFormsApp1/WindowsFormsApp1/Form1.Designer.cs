c4790d5db74ab83b8c77bca0ce49c40f126d16e4
Ji_Soo <perfumeindream@gmail.com>
2018-02-08 09:40:37 +0900

    #런타임관련 코드 수정

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,49 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Btn_OK = new System.Windows.Forms.Button();
+            this.TB_Password = new System.Windows.Forms.TextBox();
+            this.CB_Password = new System.Windows.Forms.CheckBox();
+            this.BTN_Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // button1
+            // Btn_OK
             // 
-            this.button1.Location = new System.Drawing.Point(108, 149);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Event);
+            this.Btn_OK.Location = new System.Drawing.Point(48, 97);
+            this.Btn_OK.Name = "Btn_OK";
+            this.Btn_OK.Size = new System.Drawing.Size(75, 23);
+            this.Btn_OK.TabIndex = 0;
+            this.Btn_OK.Text = "확인";
+            this.Btn_OK.UseVisualStyleBackColor = true;
+            this.Btn_OK.Click += new System.EventHandler(this.Event);
+            // 
+            // TB_Password
+            // 
+            this.TB_Password.Location = new System.Drawing.Point(84, 53);
+            this.TB_Password.Name = "TB_Password";
+            this.TB_Password.PasswordChar = '★';
+            this.TB_Password.Size = new System.Drawing.Size(100, 21);
+            this.TB_Password.TabIndex = 1;
+            this.TB_Password.Leave += new System.EventHandler(this.Event);
+            // 
+            // CB_Password
+            // 
+            this.CB_Password.AutoSize = true;
+            this.CB_Password.Location = new System.Drawing.Point(190, 55);
+            this.CB_Password.Name = "CB_Password";
+            this.CB_Password.Size = new System.Drawing.Size(60, 16);
+            this.CB_Password.TabIndex = 2;
+            this.CB_Password.Text = "보이기";
+            this.CB_Password.UseVisualStyleBackColor = true;
+            this.CB_Password.CheckedChanged += new System.EventHandler(this.Event);
+            // 
+            // BTN_Close
+            // 
+            this.BTN_Close.Location = new System.Drawing.Point(154, 97);
+            this.BTN_Close.Name = "BTN_Close";
+            this.BTN_Close.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Close.TabIndex = 3;
+            this.BTN_Close.Text = "종료";
+            this.BTN_Close.UseVisualStyleBackColor = true;
+            this.BTN_Close.Click += new System.EventHandler(this.Event);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "계정";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "비밀번호";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(95, 54);
+            this.textBox1.Location = new System.Drawing.Point(84, 15);
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '★';
             this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Leave += new System.EventHandler(this.Event);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(201, 56);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(86, 16);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.Event);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(269, 133);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTN_Close);
+            this.Controls.Add(this.CB_Password);
+            this.Controls.Add(this.TB_Password);
+            this.Controls.Add(this.Btn_OK);
             this.Name = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,9 +122,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_OK;
+        private System.Windows.Forms.TextBox TB_Password;
+        private System.Windows.Forms.CheckBox CB_Password;
+        private System.Windows.Forms.Button BTN_Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,24 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private bool sw = false;
+        List<string> MemberID;
+        MySqlConnection conn;//DB 연동을 위한 객체 생성
+        private bool sw = false;//클릭 이벤트 토글 점검용 객체
         public Form1()
         {
             InitializeComponent();
-            this.Text = "테스트폼";
-            button1.Text = "누르시오";
+            this.Text = "테스트폼";//폼 타이틀 변경
+            Btn_OK.Text = "누르시오";//버튼 텍스트 변경
+
+            #region DB연결 확인
+            bool db = DBconnection();
+            if (db)
+                DB_Read();
+            else
+                this.Close();//DB 연결 실패시 프로그램 종료
+            #endregion
         }
 
+        #region DB부
+
+        #region DB 연결
+        private bool DBconnection()
+        {
+            try
+            {
+                string strConn = "Server=localhost;Database=membermanage;Uid=root;Pwd=456123;";
+                conn = new MySqlConnection(strConn);
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("오류발생\n" + e.Message, "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+        #endregion
+
+        #region DB 쓰기
+        private void DB_Write(string ID, string PW)
+        {
+
+        }
+        #endregion
+
+        #region DB 읽기
+        private void DB_Read()
+        {
+            MemberID = new List<string>();
+            conn.Open();
+            string qry = "SELECT * From membermanage.member";
+
+            MySqlCommand cmd = new MySqlCommand(qry, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                while (rdr.Read())
+                {
+                    MemberID.Add(rdr.GetString(0));
+                }
+                rdr.Close();
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region 이벤트 핸들러
         private void Event(object sender, EventArgs e)
         {
-            if (sender is Button)
+            //object sender에 할당된 컨트롤의 종류를 검사
+            if (sender is Button)//버튼일경우
             {
                 Button seledBtn = sender as Button;
+
+                if (seledBtn.Name == "BTN_Close")
+                {
+                    //Form1_FormClosing(null, null);
+                    this.Close();
+                }                   
+
                 if (!sw)
                 {
                     MessageBox.Show("버튼이구만~!!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -39,20 +107,19 @@ namespace WindowsFormsApp1
                     MessageBox.Show("엌!!", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     this.Close();
                 }
-
             }
-            else if(sender is CheckBox)
+            else if(sender is CheckBox)//체크박스일 경우
             {
                 CheckBox selectedCB = sender as CheckBox;
                 if (selectedCB.Name == "checkBox1")
                 {
                     if (selectedCB.Checked)
-                        textBox1.PasswordChar = '\0';
+                        TB_Password.PasswordChar = '\0';
                     else
-                        textBox1.PasswordChar = '★';                    
+                        TB_Password.PasswordChar = '★';                    
                 }
             }
-            else if (sender is TextBox)
+            else if (sender is TextBox)//텍스트박스일 경우
             {
                 TextBox SeledTB = sender as TextBox;
                 if (SeledTB.TextLength == 0)
@@ -61,16 +128,18 @@ namespace WindowsFormsApp1
                     return;
                 }
             }
-            else
+            else//그 외 사실 필요 없지만(폼에 컨트롤이 버튼, 텍스트박스, 체크박스만 사용했으므로)예외처리를 위하여 추가.
             {
                 string type = sender.GetType().ToString();
-                MessageBox.Show("버튼말고 다른거구먼 " + type , "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("이거슨 " + type + "군!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-
+            if (conn.State == ConnectionState.Open)
+                conn.Close();//DB연결 해제
         }
+        #endregion
     }
 }
